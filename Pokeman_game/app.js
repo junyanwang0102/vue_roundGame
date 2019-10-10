75a38eeda6128ea898fcbf3cc000d8bf6c511c82
junyanwang0102 <48872674+junyanwang0102@users.noreply.github.com>
2019-10-11 00:07:09 +1100

    Add files via upload

---
+++ Pokeman_game/app.js
@@ -0,0 +1,107 @@
+new Vue({
+    el:'#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        turns: []
+    },
+    methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.gameIsRunning = true;
+            this.turns = [];
+        },
+        attack() {
+            let damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Player hits Monster for ${damage}`
+            })
+
+            damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Monster hits Player for ${damage}`
+            })
+
+            if(this.checkResult()) {
+                return;
+            }
+        },
+        specialAttack() {
+            let damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Player hits Monster for ${damage} with special attack.`
+            })
+
+            damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Monster hits Player for ${damage} with special attack.`
+            })
+
+            if(this.checkResult()) {
+                return
+            }
+        },
+        heal() {
+            if(this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player heals for 10`
+            })
+            // monster is still attacking while healing
+            damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage} while healing.`
+            })
+        },
+        giveUp() {
+            this.gameIsRunning = false;
+        },
+        calculateDamage(min, max) {
+            return Math.max(Math.floor(Math.random() * max), min);
+        },
+        checkResult() {
+            if(this.monsterHealth <= 0) {
+                if(confirm('You won! Start a new game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+
+            if(this.playerHealth <= 0) {
+                if(confirm('You lost! Start a new game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+});
